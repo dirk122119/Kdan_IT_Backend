8d5cb0dedb81dc58ed110470e2a0dbfd9a7bcaa9
dirk122119 <dirk122119@gmail.com>
2023-05-14 20:43:07 +0800

    fix message

--- controllers/members.js
+++ controllers/members.js
@@ -114,7 +114,7 @@ const putReClock = async (req, reply) => {
       values = [reqBody.employeeNumber, reqBody.time];
       const [result] = await connection.query(query, values);
       reply.status(201).send({
-        message: `add ${reqBody.employeeNumber} ${reqBody.checkCatagory} data at ${reqBody.time} `,
+        message: `add ${reqBody.employeeNumber} ${reqBody.checkCatagory} at ${reqBody.time} `,
       });
     } else if (rows[0][`${reqBody.checkCatagory}`] != null) {
       reply.status(409).send({
@@ -156,7 +156,7 @@ const putReClock = async (req, reply) => {
         values = [reqBody.time, reqBody.employeeNumber, date, date];
         const [result] = await connection.query(query, values);
         reply.status(200).send({
-          message: `set ${reqBody.employeeNumber}, ${reqBody.checkCatagory} data at ${reqBody.time} `,
+          message: `set ${reqBody.employeeNumber}, ${reqBody.checkCatagory} at ${reqBody.time} `,
         });
       }
     }
